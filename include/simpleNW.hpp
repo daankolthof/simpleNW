f30e28b6af4bc0d6b8247bf85effa2f8320472f5
Daan Kolthof <daan_kolthof@hotmail.com>
2014-10-25 20:35:39 +0200

    Added one single header for inclusing by user of library.

---
+++ include/simpleNW.hpp
@@ -0,0 +1,22 @@
+
+#if !defined(SIMPLENW_H)
+#define SIMPLENW_H
+
+
+#include "NetworkService.hpp"
+
+#include "ServiceOptions.hpp"
+#include "TransportProtocol.hpp"
+
+#include "Handler.hpp"
+
+#include "Server.hpp"
+#include "TCPServer.hpp"
+#include "UDPServer.hpp"
+
+#include "Connection.hpp"
+#include "TCPConnection.hpp"
+
+
+
+#endif
